59b2da17100ce7223d6dbc1cc3d547f1b267f3d0
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-01-29 16:56:19 -0800

    added NumberContainer and styled, also added GameScreen component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,15 +5,20 @@ import {
     StyleSheet, 
     Button, 
     TouchableWithoutFeedback,
-    Keyboard
+    Keyboard,
+    Alert
 } from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
+
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
 
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
 
     const numberInputHandler = inputText => {
         // validate input here
@@ -23,7 +28,36 @@ const StartGameScreen = props => {
 
     const resetInputHandler = () => {
         setEnteredValue('');
+        setConfirmed(false);
     };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert(
+                'Invalid number!', 
+                'Number has to be a number between 1 and 99', 
+                [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]);
+            return;
+        }
+        setConfirmed(true);
+        setEnteredValue(chosenNumber);
+        setSelectedNumber(parseInt(enteredValue));
+        Keyboard.dismiss();
+    };
+
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = 
+        <Card style={styles.summaryContainer}>
+            <Text>You selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="START GAME" />
+        </Card>
+        }
+
 
     return (
         <TouchableWithoutFeedback onPress={() => {
@@ -44,10 +78,23 @@ const StartGameScreen = props => {
                     value={enteredValue}
                 />
                 <View style={styles.buttonContainer}>
-                    <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Colors.secondary} /></View>
-                    <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
+                    <View style={styles.button}>
+                        <Button 
+                            title="Reset" 
+                            onPress={resetInputHandler} 
+                            color={Colors.secondary} 
+                        />
+                    </View>
+                    <View style={styles.button}>
+                        <Button 
+                            title="Confirm" 
+                            onPress={confirmInputHandler} 
+                            color={Colors.primary} 
+                        />
+                    </View>
                 </View>
                 </Card>
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
         
@@ -81,6 +128,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 });
 
