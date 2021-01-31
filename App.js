03ea51e3aef1ff2e5d6114fd41c993cf0f5834a8
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-01-31 09:57:00 -0800

    added logic to App.js with GameOverScreen component

--- App.js
+++ App.js
@@ -4,18 +4,32 @@ import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
+  // states
   const [userNumber, setUserNumber] = useState();
+  // useState(0) <-- referring to the number of rounds taken to guess num correctly
+  // starts at 0 since we havn't played any games yet
+  const [guessRounds, setGuessRounds] = useState(0);
+
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
+    // when a new game starts, want to change setGuessRounds back to 0
+    setGuessRounds(0);
   };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  }
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen />;
   }
 
   return (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,13 +3,19 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const GameOverScreen = props => {
     return (
-        <View>
-            
+        <View style={styles.screen}>
+            <Text>The Game is Over!</Text>
         </View>
     )
 };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default GameOverScreen;
